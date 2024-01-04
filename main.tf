d811037670eb9cc2bb75a38e8a78d0b7177094a8
Nicola Titmus <nicola.koo@cantab.net>
2024-01-04 09:13:23 +0000

    added main.tf file

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+
+  backend "s3" {
+    # This backend configuration is filled in automatically at test time by Terratest. If you wish to run this example
+    # manually, uncomment and fill in the config below.
+
+    bucket         = "terraform-book-state-nt2"
+    key            = "terrascan1/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "terraform-book-locks2"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+module "ec2_instance" {
+    source = "github.com/NTitmus/terrascan2?ref=v1.0.0"
+}
